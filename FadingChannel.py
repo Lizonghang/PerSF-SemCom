9c2e7f61d714c4794967a60c5c0e70e417b8b469
LI ZONGHANG <lizhuestc@gmail.com>
2022-03-15 14:52:44 +0800

    update channel env

--- FadingChannel.py
+++ FadingChannel.py
@@ -5,28 +5,56 @@ from scipy.special import gammainc
 
 
 class UserChannel:
-    def __init__(self, pid, m, ms, db, d, alpha, a, b, size):
+    def __init__(self, pid, m, ms, db, d, n, nd, pj, mi, dbi, noise, alpha, a, b, size):
         self.pid = pid
-        self.m = m
-        self.ms = ms
-        self.db = db
-        self.d = d
-        self.alpha = alpha
-        self.a = a
-        self.b = b
+        self.m = m  # multipath
+        self.ms = ms  # shadowing
+        self.db = db  # db of fading
+        self.d = d  # distance
+        self.alpha = alpha  # large scale
+        self.a = a  # modulation
+        self.b = b  # modulation
+        self.n = n  # number of antennas
+        self.nd = nd  # paths of interference
+        self.pj = pj  # power of interference (in each path)
+        self.mi = mi  # small scale of interference
+        self.dbi = dbi  # db of interference
+        self.noise = noise  # noise
         self.size = size
 
         np.random.seed(0)
         self.F = self._fisher()
+        self.I = self._inter()
 
     def _fisher(self):
         sigma = 1.
-        Xn = nakagami.rvs(self.m, loc=0, scale=np.sqrt(sigma), size=self.size)
-        A = 1 / nakagami.rvs(self.ms, loc=0, scale=np.sqrt(sigma), size=self.size)
-        R2 = pow(A, 2) * pow(Xn, 2)
+        temp = 1
         SNR_mean = pow(10, self.db / 10)
-        Y = SNR_mean * R2 / np.mean(R2)
+        Y = np.zeros(self.size)
+        Y = np.array(Y)
+        while temp <= self.n:
+            Xn = nakagami.rvs(self.m, loc=0, scale=np.sqrt(sigma), size=self.size)
+            A = 1 / nakagami.rvs(self.ms, loc=0, scale=np.sqrt(sigma), size=self.size)
+            R2 = pow(A, 2) * pow(Xn, 2)
+            T = SNR_mean * R2 / np.mean(R2)
+            Y = [T[i] + Y[i] for i in range(len(T))]
+            temp += 1
         return Y
+
+    def _inter(self):
+        sigma = 1.
+        temp = 1
+        SNR_mean = pow(10, self.dbi / 10)
+        Yi = np.zeros(self.size)
+        Yi = np.array(Yi)
+        while temp <= self.nd:
+            Xn = nakagami.rvs(self.mi, loc=0, scale=np.sqrt(sigma), size=self.size)
+            R2 = pow(Xn, 2)
+            T = SNR_mean * R2 / np.mean(R2)
+            Yi = [T[i] + Yi[i] for i in range(len(T))]
+            temp += 1
+        Yi = Yi * self.pj
+        return Yi
 
     def _nchoosek(self, n, k):
         res = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
@@ -38,7 +66,7 @@ class UserChannel:
 
         BEP = []
         for power in power_per_packet:
-            z = power * pow(self.d, -self.alpha) * self.F
+            z = [power * pow(self.d, -self.alpha) * self.F[i] / (self.noise + self.I[i]) for i in range(len(self.F))]
             z2 = (math.gamma(self.b) * (1 - gammainc(self.b, self.a * z))) \
                  / (2 * math.gamma(self.b))
             BEP.append(np.mean(z2))
@@ -63,9 +91,14 @@ class FadingChannel:
         MSs = [2, 2, 2, 2, 8, 8, 8]
         DBs = [-10, -15, -10, -10, -10, -15, -10]
         Ds = [10, 10, 10, 16, 10, 10, 16]
-
+        Ns = [3, 3, 3, 3, 3, 3, 3]
+        NDs = [3, 3, 3, 3, 3, 3, 5]
+        PIs = [2, 2, 2, 2, 8, 8, 8]
+        MIs = [2, 2, 2, 2, 2, 2, 2]
+        DBIs = [0, 0, 0, 0, 0, 0, 0]
         self.user_channels = [UserChannel(pid, Ms[pid], MSs[pid], DBs[pid], Ds[pid],
-                                          alpha=2, a=1, b=0.5, size=10000)
+                                          Ns[pid], NDs[pid], PIs[pid], MIs[pid], DBIs[pid],
+                                          noise=1, alpha=2, a=1, b=0.5, size=10000)
                               for pid in range(num_persons)]
 
     def get_user_channel(self, pid):
@@ -75,8 +108,9 @@ class FadingChannel:
 if __name__ == "__main__":
     num_persons = 7
     fading_channel = FadingChannel(num_persons)
+
     for pid in range(num_persons):
         user_channel = fading_channel.get_user_channel(pid)
         drop_prob = user_channel.get_packet_drop_prob(
-            8000, [0.1, 0.2, 0.3, 0.4, 0.5 ,0.6, 0.7])
+            8000, [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7])
         print(drop_prob)
